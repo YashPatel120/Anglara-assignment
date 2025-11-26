7ff794fbc9e918bc7786e310a0de6a9ef304f12e
YashPatel120 <gamdhayash@gmail.com>
2025-11-26 10:19:32 +0530

    Auth issue solved

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export default function(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ msg: 'No token' });
+export default function (req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ msg: "No token" });
+
+  const token = authHeader.split(" ")[1]; // remove "Bearer"
+
   try {
-    jwt.verify(token, 'secret');
+    jwt.verify(token, "secret");
     next();
-  } catch {
-    res.status(401).json({ msg: 'Invalid token' });
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
   }
 }
